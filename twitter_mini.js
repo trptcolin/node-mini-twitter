9a9e7fe0c2e72df595aa48e66f765a1b2a3e475d
Colin Jones <trptcolin@gmail.com>
2010-08-05 23:36:27 -0500

    update timeline from the homepage

--- twitter_mini.js
+++ twitter_mini.js
@@ -19,14 +19,27 @@ var Twitter = {
   apiHost: 'api.twitter.com',
 
   timelineUrl: "/1/statuses/friends_timeline.json",
+  updateUrl: "/1/statuses/update.json",
 
-  getTimeline: function(onComplete) {
+  makeRequest: function(method, url, params, callback) {
     var client = http.createClient(80, this.apiHost);
     var headers = {'host': this.apiHost,
                    'Authorization': 'Basic ' + encodeCreds(this.creds)};
-    var request = client.request('GET', this.timelineUrl, headers);
+    var request = client.request(method, url, headers);
+    request.write(params);
     request.end();
+    callback(request);
+  },
 
+  get: function(url, callback) {
+    this.makeRequest("GET", url, [], callback);
+  },
+
+  post: function(url, params, callback) {
+    this.makeRequest("POST", url, params, callback);
+  },
+
+  responder: function(request, onComplete) {
     var chunks = "";
     request.on('response', function (response) {
       var chunks = "";
@@ -40,6 +53,20 @@ var Twitter = {
     });
   },
 
+  getTimeline: function(onComplete) {
+    var that = this;
+    this.get(this.timelineUrl, function(request) {
+      that.responder(request, onComplete);
+    });
+  },
+
+  postUpdate: function(params, onComplete) {
+    var that = this;
+    this.post(this.updateUrl, params, function(request) {
+      that.responder(request, onComplete);
+    });
+  },
+
   linkForUser: function(screenName, text) {
     return "<a href='http://twitter.com/" + screenName + "'>" + text + "</a>";
   },
@@ -47,37 +74,76 @@ var Twitter = {
   htmlForTweet: function(tweet) {
     var user = tweet["user"];
     var screenName = user["screen_name"];
-    var avatar = this.linkForUser(screenName, "<img src='" + user["profile_image_url"] + "' />");
+    var avatar = this.linkForUser(screenName, "<img class='avatar' src='" + user["profile_image_url"] + "' />");
     var htmlScreenName = "<h3>" + this.linkForUser(screenName, screenName) + "</h3>";
     var htmlText = "<p>" + tweet["text"] + "</p>";
     return "<div class='tweet'>" + avatar + htmlScreenName + htmlText + "</div>";
   }
-}
+};
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-
-  var htmlFor = function(content) {
+var MiniTwitter = {
+  htmlFor: function(content) {
     var css = "* {padding: 0; margin: 0;}" +
               "html {background-color: #FAFAFA;}" +
               "h3 {display: inline;}" +
               "img {vertical-align: middle; padding-right: 10px;}" +
+              "textarea {padding: 3px;}" +
+              ".avatar {border: none; height: 48px; width: 48px;}" +
               ".content {width: 80%; margin: 0 auto;}" +
               ".tweet {padding: 5px; background: #FAFAFA; border-bottom: 1px solid #AAA;}";
+
     var html = "<html><head><title>Twitter Mini</title><style type='text/css'>" +
                css + '<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />' +
                "</style></head><body><div class='content'><h1>Twitter Mini</h1>" + content + "</div></body></html>"
 
     return html;
+  },
+
+  index: function (res) {
+    var updateForm = '<form action="/update"' +
+                     ' class="status-update-form" id="status_update_form" method="post">' +
+                     '<textarea cols="40" rows="3" id="status" name="status"></textarea><br />' +
+                     '<input type="submit" value="Mini-Tweet it!" />' +
+                     '</form>';
+
+    Twitter.getTimeline(function(content) {
+      var data = _.map(JSON.parse(content), function(tweet) {
+        return Twitter.htmlForTweet(tweet);
+      });
+
+      res.end(MiniTwitter.htmlFor(updateForm + data.join("")));
+    });
+  },
+
+  update: function (req, data, res) {
+    var that = this;
+    Twitter.postUpdate(data, function(content) {
+      that.index(res);
+    });
+  }
+};
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  if (req.url === "/") {
+    MiniTwitter.index(res);
+  }
+  else if (req.url === "/update") {
+    var chunks = "";
+
+    req.on('data', function(chunk) {
+      chunks += chunk;
+    });
+
+    req.on('end', function() {
+      MiniTwitter.update(req, chunks, res);
+    });
+  }
+  else {
+    res.end("BOGUS");
   }
 
-
-  Twitter.getTimeline(function(content) {
-    var data = _.map(JSON.parse(content), function(tweet) {
-      return Twitter.htmlForTweet(tweet);
-    });
-    res.end(htmlFor(data.join("")));
-  });
 }).listen(8124, "127.0.0.1");
 
 console.log('Server running at http://127.0.0.1:8124/');
